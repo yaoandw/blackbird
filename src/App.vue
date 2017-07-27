506626e44e1e29528958092366c7b1a1d6ab7397
yaoandw <yaoandw@163.com>
2017-07-27 16:26:22 +0800

    cases

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
     ],
     link: [
-      { rel: 'stylesheet', href: '/css/index.css' },
+//      { rel: 'stylesheet', href: '/css/index.css' },
 //          { rel: 'favicon', href: 'favicon.ico' },
       { rel: 'shortcut icon', type: 'image/png', href: '/static/favicon.png' },
     ],
