8be9737daad6be7f9edf4e720674bf3d3897d0c5
yaoandw <yaoandw@163.com>
2017-08-16 16:02:43 +0800

    add build dir

--- src/http-common.js
+++ src/http-common.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 import sess from './sess';
 
 export const HTTP = axios.create({
-  baseURL: 'http://www.jojo.la:8080/',
+  // baseURL: 'http://www.jojo.la:8080/',
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://www.jojo.la:8080/' : 'http://127.0.0.1:8080',
   // baseURL: 'https://www.jojo.la/',
   rejectUnauthorized: false,
   timeout: 1000,
